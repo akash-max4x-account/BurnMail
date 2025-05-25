e18b85fb7e19327707fa54b552cba5ef056e4974
Lorenzo <hello@lorenzo0111.me>
2025-05-25 12:35:26 +0200

    Added privacy and tos

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -2,11 +2,27 @@ import { Heart } from "lucide-react";
 
 export function Footer() {
   return (
-    <footer className="flex text-center gap-1 mt-auto w-full justify-center items-center">
-      Made with <Heart className="text-primary" /> by{" "}
-      <a href="https://lorenzo0111.me" target="_blank" className="text-primary">
-        Lorenzo0111
-      </a>
+    <footer className="flex flex-col text-center gap-1 mt-auto w-full justify-center items-center">
+      <p className="flex text-center gap-1 w-full justify-center items-center">
+        Made with <Heart className="text-primary" /> by{" "}
+        <a
+          href="https://lorenzo0111.me"
+          target="_blank"
+          className="text-primary"
+        >
+          Lorenzo0111
+        </a>
+      </p>
+
+      <div className="flex text-center gap-3 w-full justify-center items-center">
+        <a href="https://to.lorenzo0111.me/burnpp" className="text-sm text-gray-500">
+          Privacy Policy
+        </a>
+
+        <a href="https://to.lorenzo0111.me/burntos" className="text-sm text-gray-500">
+          Terms of Service
+        </a>
+      </div>
     </footer>
   );
 }
